0d058c68c278150b88c3a33c460c7ea39c97f08f
Leonardo <leoonardoogzz@gmail.com>
2024-09-15 23:00:29 -0300

    Error en el get por id

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -18,15 +18,36 @@ export const createUserCtrl = async (req, res) => {
 
   try {
     const [result] = await conn.execute(
-      "INSERT INTO `users`(`name`, `email`, `password`) VALUES ('?','?','?');",
+      "INSERT INTO `users`(`name`, `email`, `password`) VALUES (?,?,?);",
       [name, email, password]
     );
 
-    console.log(result);
+    const [userFinded] = await conn.execute(
+      "SELECT * FROM users WHERE id = ?",
+      [result.insertId]
+    );
 
-    res.status(201).json({
-      msg: "Usuario creado con exito",
+    res.status(201).json({ userFinded });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "Hubo un error en la base de datos",
     });
+  }
+};
+
+//GET /users/:id
+export const findUserByIdCtrl = async (req, res) => {
+  const userId = Number(req.params.id);
+
+  try {
+    const [findedUser] = await conn.execute(
+      "SELECT * FROM `users` WHERE id = ?;",
+      [userId]
+    );
+
+    res.status(404).json(findedUser[0]);
   } catch (error) {
     console.log(error);
 
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import {
   allUserCtrl,
   createUserCtrl,
+  findUserByIdCtrl,
 } from "../controllers/user.controllers.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", allUserCtrl);
 userRoutes.post("/", createUserCtrl);
-userRoutes.get("/id:");
+userRoutes.get("/id:", findUserByIdCtrl);
 userRoutes.patch("/:id");
 userRoutes.delete("/:id");
 
