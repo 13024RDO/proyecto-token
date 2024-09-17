f7f63b595529794a92a2ca5f885f78a34a50b22f
Leonardo <leoonardoogzz@gmail.com>
2024-09-16 23:43:19 -0300

    Se termino el crud

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -47,12 +47,76 @@ export const findUserByIdCtrl = async (req, res) => {
       [userId]
     );
 
-    res.status(404).json(findedUser[0]);
+    if (findedUser.length < 1) {
+      return res.status(404).json({
+        msg: "User not found",
+      });
+    }
+
+    res.status(200).json(findedUser[0]);
   } catch (error) {
     console.log(error);
 
     res.status(500).json({
       msg: "Hubo un error en la base de datos",
+    });
+  }
+};
+
+//PATCH /users/:id
+export const updateUserCtrl = async (req, res) => {
+  const userId = +req.params.id;
+  const { name, email, password } = req.body;
+
+  try {
+    const [result] = await conn.execute(
+      "UPDATE users SET name=?, email=?, password=? WHERE  id = ?",
+      [name, email, password, userId]
+    );
+
+    if (result.affectedRows == 0) {
+      return res.status(404).json({
+        msg: "User not found",
+      });
+    }
+
+    const findedUser = await conn.execute("SELECT * FROM  users WHERE id = ?", [
+      userId,
+    ]);
+
+    res.status(200).json(findedUser[0]);
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "Hubo un error en la base de datos",
+    });
+  }
+};
+
+//DELETE /users/:id
+export const deleteUserCtrl = async (req, res) => {
+  const userId = +req.params.id;
+
+  try {
+    const [result] = await conn.execute("DELETE FROM users WHERE id = ?", [
+      userId,
+    ]);
+
+    if (result.affectedRows == 0) {
+      return res.status(404).json({
+        msg: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      msg: "User delete success",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "Hubo un error con la base de datos",
     });
   }
 };
--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -2,15 +2,17 @@ import { Router } from "express";
 import {
   allUserCtrl,
   createUserCtrl,
+  deleteUserCtrl,
   findUserByIdCtrl,
+  updateUserCtrl,
 } from "../controllers/user.controllers.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", allUserCtrl);
 userRoutes.post("/", createUserCtrl);
-userRoutes.get("/id:", findUserByIdCtrl);
-userRoutes.patch("/:id");
-userRoutes.delete("/:id");
+userRoutes.get("/:id", findUserByIdCtrl);
+userRoutes.patch("/:id", updateUserCtrl);
+userRoutes.delete("/:id", deleteUserCtrl);
 
 export { userRoutes };
